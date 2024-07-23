8df1eeee30f6e5e92fb79bc279bd63dca7ed85d6
thebhaveshshinde23 <thebhaveshshinde@gmail.com>
2024-07-23 22:24:20 +0530

    Updated index1.js file

--- index1.js
+++ index1.js
@@ -1,27 +1,35 @@
 const Array_object = [
     {
-        id:1,
-        name:"Bhavesh",
-        gunc:function(){
+        id: 1,
+        name: "Bhavesh",
+        gunc: function () {
             console.log(`Hii name is ${this.name} and id is ${this.id}`)
-        //    return console.log (this)
+            //    return console.log (this)
         }
     },
     {
-        id:2,
-        name:"Arun",
-        gunc:function(){
+        id: 2,
+        name: "Arun",
+        gunc: function () {
             console.log(`Hii name is ${this.name} and id is ${this.id}`)
             // console.log(this)
         }
     },
     {
-        id:3,
-        name:"Shinde",
-        gunc:function(){
+        id: 3,
+        name: "Shinde",
+        gunc: function () {
+            console.log(`Hii name is ${this.name} and id is ${this.id}`)
+            // console.log(this)
+        }
+    },
+    {
+        id: 4,
+        name: "Rohan",
+        gunc: function () {
             console.log(`Hii name is ${this.name} and id is ${this.id}`)
             // console.log(this)
         }
     }
 ]
- module.exports = Array_object
+module.exports = Array_object
